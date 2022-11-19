f912329d515b07bff5b052cb6747eaf0d2ce10dd
user_ddo <@gmail.com>
2022-11-19 16:45:43 +0300

    ADDED MODELS

--- lms/admin.py
+++ lms/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from lms.models import Curator, Direction
+from lms.models import Curator, Direction, Disciplina, Group, Student
 
 admin.site.register(Curator)
 admin.site.register(Direction)
+admin.site.register(Disciplina)
+admin.site.register(Group)
+admin.site.register(Student)
--- lms/models.py
+++ lms/models.py
@@ -11,3 +11,31 @@ class Direction(models.Model):
     curator = models.ForeignKey(Curator,
                                 on_delete=models.CASCADE ,
                                 related_name='direction')
+
+class Group(models.Model):
+    number = models.CharField(max_length=20)
+    deviz = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    direction = models.ForeignKey(Direction,
+                                  on_delete=models.CASCADE,
+                                  related_name='group')
+    def __str__(self):
+        return 'group '+self.number +'('+str(self.id)+')'
+
+class Disciplina(models.Model):
+    name = models.CharField(max_length=50)
+    lessons = models.CharField(max_length=20)
+    homework = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    direction = models.ForeignKey(Direction,
+                                  on_delete=models.CASCADE,
+                                  related_name='disciplina')
+
+class Student(models.Model):
+    surname = models.CharField(max_length=50)
+    name = models.CharField(max_length=20)
+    otchesvo = models.CharField(max_length=50)
+    created_date = models.DateTimeField(default=timezone.now)
+    group = models.ForeignKey(Group,
+                                  on_delete=models.CASCADE,
+                                  related_name='student')
